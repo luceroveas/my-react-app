f1e5e79dc26832ac56c6c906182256c8cbe5b7e5
luceroveas <143043562+luceroveas@users.noreply.github.com>
2024-04-15 18:50:18 -0600

    Added axios integration so it shows the weather of a city

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Helloworld</h1>
+        <Weather city="Tokyo" />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import LoadingOverlay from "react-loading-overlay-nextgen";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in  ${response.data.name} is ${response.data.main.temp} C `
+    );
+  }
+  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <LoadingOverlay active={true} spinner text="wait a bit...">
+      <p>Some content or children or something.</p>
+    </LoadingOverlay>
+  );
+}

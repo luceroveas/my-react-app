260721455bd3c15e41dfa2935671ab6adaf184bb
luceroveas <143043562+luceroveas@users.noreply.github.com>
2024-04-15 19:06:20 -0600

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -12,5 +12,5 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h1> Helloworld from weather.js</h1>;
+  return <h1> Helloworld from weather.jsS</h1>;
 }

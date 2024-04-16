fe71cef4244b6fa0a9020655d9142ff9688a48c5
luceroveas <143043562+luceroveas@users.noreply.github.com>
2024-04-15 20:29:13 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
-        <small>
-          <a href="https://github.com/luceroveas/my-react-app">
-            {" "}
-            Open-source code{" "}
-          </a>
-          , by Andrea Vea from <a href="https://www.shecodes.io/">She Codes </a>
-        </small>
       </header>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://main--fastidious-druid-af1a2f.netlify.app/">
+          {" "}
+          Andrea Vea{" "}
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/luceroveas/my-react-app">
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }

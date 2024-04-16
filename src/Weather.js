c53366660ec9baff953b37cdc8becb7082f00a5f
luceroveas <143043562+luceroveas@users.noreply.github.com>
2024-04-15 19:14:17 -0600

    Move CodeSandboxReact project to VS code

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in  ${response.data.name} is ${response.data.main.temp} C `
-    );
-  }
-  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
-  axios.get(apiUrl).then(handleResponse);
-  return <h1> Helloworld from weather.jsS</h1>;
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C </li>
+          <li>Description: {weather.description} </li>
+          <li>Humidity: {weather.humidity}% </li>
+          <li>Wind: {Math.round(weather.wind)} km/h </li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }

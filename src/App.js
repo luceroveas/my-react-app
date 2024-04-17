98bf5c8e8b025625bdcc77626725368b3e0be687
luceroveas <143043562+luceroveas@users.noreply.github.com>
2024-04-16 21:47:00 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-      </header>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://main--fastidious-druid-af1a2f.netlify.app/">
-          {" "}
-          Andrea Vea{" "}
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/luceroveas/my-react-app">
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <Weather />
+        </header>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://main--fastidious-druid-af1a2f.netlify.app/">
+            {" "}
+            Andrea Vea{" "}
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/luceroveas/my-react-app">
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
